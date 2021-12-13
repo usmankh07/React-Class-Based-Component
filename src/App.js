48538bcf61c65e87bffb959f233e7f70ecf2a500
Usman Khan <uk03777@gmail.com>
2021-12-13 16:23:04 +0500

    4th commit to master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Home from './components/Home';
+// import Home from './components/Home';
+import News from './components/News';
 
 function App() {
   return (
     <div className="App">
-   <h3>This is Usman.</h3>
-   <Home desc="This is our description"/>
+   {/* <h3>This is Usman.</h3>
+   <Home desc="This is our description"/> */}
+   <News />
 
     </div>
   );
